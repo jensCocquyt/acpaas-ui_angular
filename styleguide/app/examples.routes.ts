37f9a675d503dd432768b12b489e2113f437e4f7
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-20 12:29:05 +0200

    changes

--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -1,36 +1,23 @@
-<<<<<<< HEAD
-import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar';
-import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar';
-import { FLYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/flyout';
-import { LOCALSTORAGE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/localstorage';
-import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo';
-import { PAGINATION_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/pagination';
-import { PROGRESS_BAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/progress-bar';
-import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list';
-=======
 import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar/fesm2015/avatar';
 import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar/fesm2015/calendar';
 import { FLYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/flyout/fesm2015/flyout';
 import { FORMS_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/forms/fesm2015/forms';
 import { LAYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/layout/fesm2015/layout';
+import { LOCALSTORAGE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/localstorage/fesm2015/localstorage';
 import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
 import { MAP_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/map/fesm2015/map';
 import { PAGINATION_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/pagination/fesm2015/pagination';
 import { PROGRESS_BAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/progress-bar/fesm2015/progress-bar';
 import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
 import { TABLE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/table/fesm2015/table';
->>>>>>> master
 
 export const EXAMPLES_ROUTES = [
 	{ path: 'avatar', children: AVATAR_EXAMPLES_ROUTES, title: 'Avatar', },
 	{ path: 'calendar', children: CALENDAR_EXAMPLES_ROUTES, title: 'Calendar', },
 	{ path: 'flyout', children: FLYOUT_EXAMPLES_ROUTES, title: 'Flyout', },
-<<<<<<< HEAD
-	{ path: 'localstorage', children: LOCALSTORAGE_EXAMPLES_ROUTES, title: 'Localstorage', },
-=======
 	{ path: 'forms', children: FORMS_EXAMPLES_ROUTES, title: 'Forms', },
 	{ path: 'layout', children: LAYOUT_EXAMPLES_ROUTES, title: 'Layout', },
->>>>>>> master
+	{ path: 'localstorage', children: LOCALSTORAGE_EXAMPLES_ROUTES, title: 'Localstorage', },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES, title: 'Logo', },
 	{ path: 'map', children: MAP_EXAMPLES_ROUTES, title: 'Map', },
 	{ path: 'pagination', children: PAGINATION_EXAMPLES_ROUTES, title: 'Pagination', },
